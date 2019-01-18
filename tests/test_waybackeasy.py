0a4506ab0e5b783124b406fad7a6c2894319dbaf
Johannes Seikowsky <Johannes@Josiannes-MacBook-Pro.local>
2019-01-18 03:12:13 +0000

    tests

--- tests/test_waybackeasy.py
+++ tests/test_waybackeasy.py
@@ -2,29 +2,27 @@
 import unittest
 from ..waybackeasy import *
 
-
 class Tests(unittest.TestCase):
-	""" Testing our main function in Waybackeasy """
+	""" some basic testing of waybackeasy get function """
 
-	# checking whether normal retrieval from WB-machine works	
-	def test_something(self):		
+	# checking normal retrieval from WB-machine	
+	def test_get_func(self):		
 		for each in ["https://news.ycombinator.com", "news.ycombinator.com", "https://news.ycombinator.com/"]:
 			result = get(each, "27.05.2016")
 			if "INTERNET ARCHIVE ON" in result:
 				wbmachine_check = True
 			self.assertTrue(wbmachine_check)
 
-	# checking Errors
-	def test_url_format_error(self):
+	# checking appropriate Error rendering
+	def test_url_format_errors(self):
 		self.assertRaises(FormatError, get, "ttttt", "27.05.2016")
 
-	def test_date_format_error(self):
+	def test_date_format_errors(self):
 		self.assertRaises(FormatError, get, "https://news.ycombinator.com", "2705.20166")
-
 
 """
 way to run test:
-cd into first waybackeasy directory -- that which includes setup.py etc.
+cd into first waybackeasy directory, that which includes setup.py etc.
 then run: python -m unittest waybackeasy.tests.test_waybackeasy
--- due to Python's weiredness with importing sibling/parent modules
+-- this is due to Python's weiredness with importing modules from sibling/parent directories. odd.
 """
